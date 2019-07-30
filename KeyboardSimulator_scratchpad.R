67f19770032d8150d3bb7203a24adb64493a2d13
sdevine188 <sdevine188@gmail.com>
2019-07-30 15:37:05 -0400

    updated

---
+++ KeyboardSimulator_scratchpad.R
@@ -0,0 +1,13 @@
+library(KeyboardSimulator)
+# https://github.com/ChiHangChen/KeyboardSimulator
+
+# open a word doc and put it on right side of screen for test
+mouse.move(960, 540)
+mouse.click()
+keybd.press("a")
+keybd.press("enter")
+keybd.press("a")
+keybd.press("1")
+keybd.press("2")
+keybd.press("3")
+
